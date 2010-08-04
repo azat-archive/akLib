30aa9fcc8f93ed6f109d1a72f63570b6f142f6ff
Azat Khuzhin <dohardgopro@gmail.com>
2010-08-04 14:23:37 +0000

    Add licence

--- akDispatcher/example.php
+++ akDispatcher/example.php
@@ -1,5 +1,12 @@
 <?
 
+/*
+ * This file is part of the akLib package.
+ * (c) 2010 Azat Khuzhin <dohardgopro@gmail.com>
+ *
+ * For the full copyright and license information, please view http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
 /**
  * Exapmle of akDispatcher
  * 
